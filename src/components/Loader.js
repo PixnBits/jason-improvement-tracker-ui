170ea7a834e60c009d154fcdb25a425d73237494
Nick Oliver <github@pixnbits.org>
2016-02-19 09:56:29 -0700

    loop over the loader orbs

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,23 +2,21 @@ import React from 'react'
 import classes from './Loader.scss'
 
 function Loader () {
+
   return (
 	<div className={classes.windows8}>
-        <div className={classes.wBall} id={classes['wBall_1']}>
-            <div className={classes.wInnerBall}></div>
-        </div>
-        <div className={classes.wBall} id={classes['wBall_2']}>
-            <div className={classes.wInnerBall}></div>
-        </div>
-        <div className={classes.wBall} id={classes['wBall_3']}>
-            <div className={classes.wInnerBall}></div>
-        </div>
-        <div className={classes.wBall} id={classes['wBall_4']}>
-            <div className={classes.wInnerBall}></div>
-        </div>
-        <div className={classes.wBall} id={classes['wBall_5']}>
-            <div className={classes.wInnerBall}></div>
-        </div>
+      {
+        Object
+          .keys(classes)
+          .filter(n => /^wBall_/.test(n))
+          .map(n => {
+            return (
+              <div className={classes.wBall} id={classes[n]}>
+                  <div className={classes.wInnerBall}></div>
+              </div>
+            )
+          })
+      }
     </div>
   )
 }
